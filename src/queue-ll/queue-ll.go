bed3528f273e92a7c3049ffd9d660b6b43bb6339
Harshad Saykhedkar <harshad.say@gmail.com>
2014-09-15 19:52:36 +0530

    Formatting queue implementation

--- src/queue-ll/queue-ll.go
+++ src/queue-ll/queue-ll.go
@@ -3,19 +3,21 @@ package queue_ll
 
 //import "fmt"
 
-type node struct{
-    val interface{}
-    previous *node
-    next *node}
+type node struct {
+	val      interface{}
+	previous *node
+	next     *node
+}
 
-type Queue struct{
-    head *node
-    tail *node
-    len int }
+type Queue struct {
+	head *node
+	tail *node
+	len  int
+}
 
-func (s *Queue) Len() int {return s.len }
+func (s *Queue) Len() int { return s.len }
 
-func (s *Queue) IsEmpty() bool {return s.head == nil }
+func (s *Queue) IsEmpty() bool { return s.head == nil }
 
 //Function Push pushes the given value at the end of queue. If the queue is
 //empty, create a new node, set head and tail pointers to that node.
@@ -24,21 +26,21 @@ func (s *Queue) IsEmpty() bool {return s.head == nil }
 //new node. Set the queue's tail to point to this new node.
 //Attempting to insert nil value panics.
 func (s *Queue) Push(val interface{}) {
-    if s.IsEmpty() {
-        tmp := new(node)
-        tmp.val = val
-        s.head = tmp
-        s.tail = tmp
-        s.len += 1
-    } else {
-        current_tail := s.tail
-        tmp := new(node)
-        tmp.val = val
-	current_tail.previous = tmp
-        tmp.next = current_tail
-        s.tail = tmp
-        s.len += 1
-    }
+	if s.IsEmpty() {
+		tmp := new(node)
+		tmp.val = val
+		s.head = tmp
+		s.tail = tmp
+		s.len += 1
+	} else {
+		current_tail := s.tail
+		tmp := new(node)
+		tmp.val = val
+		current_tail.previous = tmp
+		tmp.next = current_tail
+		s.tail = tmp
+		s.len += 1
+	}
 }
 
 //Function Pop pops one item from the front of queue. If the queue is empty
@@ -46,12 +48,13 @@ func (s *Queue) Push(val interface{}) {
 //If the queue is not empty, get the value from the node at the head. Set
 //the head to point to the previous node of current head
 func (s *Queue) Pop() interface{} {
-                if s.IsEmpty(){
-                   panic("blah")
-                } else {
-                   current_head := s.head
-                   val := current_head.val
-                   s.head = current_head.previous
-                   current_head.val = nil 
-                   return val }
+	if s.IsEmpty() {
+		panic("Pop on empty queue")
+	} else {
+		current_head := s.head
+		val := current_head.val
+		s.head = current_head.previous
+		current_head.val = nil
+		return val
+	}
 }
--- src/queue-ll/queue-ll_test.go
+++ src/queue-ll/queue-ll_test.go
@@ -1,70 +1,72 @@
 package queue_ll
 
-import ("testing")
+import (
+	"testing"
+)
 
 func TestIsEmptyOnEmptyQueue(t *testing.T) {
-    tmp := new(Queue)
-    if tmp.IsEmpty() {
-        t.Log("IsEmpty on empty queue works as expected") 
-    } else {
-        t.Fail() 
-    }
+	tmp := new(Queue)
+	if tmp.IsEmpty() {
+		t.Log("IsEmpty on empty queue works as expected")
+	} else {
+		t.Fail()
+	}
 }
 
 func TestIsEmptyOnNonEmptyQueue(t *testing.T) {
-    tmp := new(Queue)
-    tmp.Push(1)
-    if !tmp.IsEmpty() {
-        t.Log("IsEmpty on non empty queue works as expected") 
-    } else {
-        t.Fail() 
-    }
+	tmp := new(Queue)
+	tmp.Push(1)
+	if !tmp.IsEmpty() {
+		t.Log("IsEmpty on non empty queue works as expected")
+	} else {
+		t.Fail()
+	}
 }
 
 func TestQueueFuctionality(t *testing.T) {
-    tmp := new(Queue)
-    tmp.Push(1)
-    tmp.Push(2)
-    tmp.Push(3)
-    tmp.Push(4)
-    first := tmp.Pop()
-    second := tmp.Pop()
-    third := tmp.Pop()
-    fourth := tmp.Pop()
-    //fmt.Println(first, second, third, fourth)
-    if (first == 1) && (second == 2) && (third == 3) && (fourth == 4) {
-        t.Log("queue push and pop works as expected")
-    } else {
-        t.Fail()
-   }
+	tmp := new(Queue)
+	tmp.Push(1)
+	tmp.Push(2)
+	tmp.Push(3)
+	tmp.Push(4)
+	first := tmp.Pop()
+	second := tmp.Pop()
+	third := tmp.Pop()
+	fourth := tmp.Pop()
+	//fmt.Println(first, second, third, fourth)
+	if (first == 1) && (second == 2) && (third == 3) && (fourth == 4) {
+		t.Log("queue push and pop works as expected")
+	} else {
+		t.Fail()
+	}
 }
 
-func TestPopOnEmptyQueuePanics(t *testing.T){
-    defer func(){
-        if r := recover() ; r != nil {
-            t.Log("popping from empty queue panics, as expected")
-          }
-      }()
-    tmp := new(Queue)
-    tmp.Pop()
+func TestPopOnEmptyQueuePanics(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Log("popping from empty queue panics, as expected")
+		}
+	}()
+	tmp := new(Queue)
+	tmp.Pop()
 }
 
-func TestLenOnEmptyQueue(t *testing.T){
-    tmp := new(Queue)
-    if tmp.Len() == 0 {
-        t.Log("len works on empty queue as expected")
-    } else {
-        t.Fail()
-    }
+func TestLenOnEmptyQueue(t *testing.T) {
+	tmp := new(Queue)
+	if tmp.Len() == 0 {
+		t.Log("len works on empty queue as expected")
+	} else {
+		t.Fail()
+	}
 }
 
-func TestLenOnNonEmptyQueue(t *testing.T){
-    tmp := new(Queue)
-    tmp.Push(1)
-    tmp.Push(2)
-    if tmp.Len() == 2 {
-       t.Log("len works on non empty queue as expected")
-    } else {
-       t.Fail() 
-   }
+func TestLenOnNonEmptyQueue(t *testing.T) {
+	tmp := new(Queue)
+	tmp.Push(1)
+	tmp.Push(2)
+	if tmp.Len() == 2 {
+		t.Log("len works on non empty queue as expected")
+	} else {
+		t.Fail()
+	}
 }
